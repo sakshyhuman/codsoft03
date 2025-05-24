629b2a8464e3905dfe35459e3ad5217ce44a839e
Sakshi Suman <risakshi01@gmail.com>
2025-05-25 00:59:59 +0530

    E-comerece

---
+++ e-comerce/login.js
@@ -0,0 +1,12 @@
+function validate() {
+    var username = document.getElementById("username").value;
+    var username = document.getElementById("password").value;
+    if (isdigit(x) || x < 1 || x > 100) {
+        text = "login successful"; <
+        a href = "page3.html" > < /a>
+
+
+    } else if (isNaN(x) || x < 1 || x > 10) {
+        text = "login failed";
+    }
+}
